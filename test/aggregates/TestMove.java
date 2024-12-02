849924fc634f88e8a5ee4f02bed572c7319176aa
stevengeorge128 <stevengeorge@arizona.edu>
2024-12-02 16:52:37 -0700

    Added test file for move. Sorry didnt mean to do main branch

---
+++ test/aggregates/TestMove.java
@@ -0,0 +1,14 @@
+package aggregates;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestMove {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
